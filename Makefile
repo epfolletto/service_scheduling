afa0c42cee40018195717de6d9c52f50fbbf39fc
Evandro Paulo Folletto <evandrofolletto@gmail.com>
2024-06-18 08:53:47 -0300

    create class CustomUser

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 run:
 	poetry run python3 manage.py runserver
+mmg:
+	poetry run python3 manage.py makemigrations
+migrate:
+	poetry run python3 manage.py migrate
--- service_scheduling/settings.py
+++ service_scheduling/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_filters",
+    "rest_framework",
+    "appointment",
     "user",
 ]
 
@@ -104,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -122,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'user.CustomUser'
